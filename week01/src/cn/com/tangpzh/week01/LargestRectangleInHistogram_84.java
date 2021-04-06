15cbabbd24f2bc3c80ceb282d25f149b79757dde
chta124 <tpz1124@126.com>
2021-04-06 11:47:23 +0800

    84. 柱状图中最大的矩形

--- week01/src/cn/com/tangpzh/week01/LargestRectangleInHistogram_84.java
+++ week01/src/cn/com/tangpzh/week01/LargestRectangleInHistogram_84.java
@@ -4,21 +4,21 @@ import java.util.Stack;
 
 /**
  * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
- *
+ * <p>
  * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
- *
+ * <p>
  * 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
- *
- *
+ * <p>
+ * <p>
  * 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: [2,1,5,6,2,3]
  * 输出: 10
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -26,12 +26,62 @@ import java.util.Stack;
 public class LargestRectangleInHistogram_84 {
 
     public static void main(String[] args) {
-        int[] heights = new int[]{2,1,5,6,2,3};
+//        int[] heights = new int[]{2, 1, 5, 6, 2, 3};
+        int[] heights = new int[]{1, 1};
+        System.out.println(LargestRectangleInHistogram_84.method1(heights));
         System.out.println(LargestRectangleInHistogram_84.method2(heights));
     }
 
     /**
+     * 暴力法
+     * 两层for loop
+     *
+     * @param heights
+     * @return
+     */
+    public static int method1(int[] heights) {
+        if (heights == null || heights.length <= 0) {
+            return 0;
+        }
+        if (heights.length == 1) {
+            return heights[0];
+        }
+
+        int area = 0;
+        int len = heights.length;
+        for (int i = 0; i < len; i++) {
+            // 固定高度
+            int h = heights[i];
+            // 遍历宽度
+            // 向左遍历
+            int L_Bound = i;
+            int R_Bound = i;
+            for (int L = i - 1; L >= 0; L--) {
+                if (heights[L] >= heights[i]) {
+                    L_Bound = L;
+                }else{
+                    break;
+                }
+            }
+            // 向右遍历
+            for (int R = i + 1; R < len; R++) {
+                if (heights[R] >= heights[i]) {
+                    R_Bound = R;
+                }else{
+                    break;
+                }
+            }
+
+            area = Math.max(area, (R_Bound - L_Bound + 1) * h);
+        }
+
+        return area;
+    }
+
+
+    /**
      * 使用【单调栈】来处理
+     *
      * @param heights
      * @return
      */
